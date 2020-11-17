e51eb01f518754cd5c3ff59a88349c909fbcd3bf
Joseph McGrath <joseph.mcgrath@engineer.com>
2020-11-17 16:51:48 +0000

    Updates

---
+++ precache-manifest.dedd92a082e3599c9a28e81567953bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98d8a2a55456b37edb0f233fe173759",
+    "url": "/punk-api/index.html"
+  },
+  {
+    "revision": "b4f7846e4b5853b6b183",
+    "url": "/punk-api/static/css/main.7d2e2dd6.chunk.css"
+  },
+  {
+    "revision": "7c91e9e83f233c60e647",
+    "url": "/punk-api/static/js/2.aaf24d7a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/punk-api/static/js/2.aaf24d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f7846e4b5853b6b183",
+    "url": "/punk-api/static/js/main.9fabb2bb.chunk.js"
+  },
+  {
+    "revision": "923fd30dfe62d76bf30e",
+    "url": "/punk-api/static/js/runtime-main.4e3fd700.js"
+  }
+]);
